b8bfe0f33a0329a8488e4099f13abe1766428c3f
Pulkit Methaniya <79719013+Pulkit2099@users.noreply.github.com>
2025-07-17 15:48:05 +0530

    Update page.js

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,13 @@
 import Image from "next/image";
 import HeroSection from "./components/HeroSection";
 import Form from "./components/Form";
+import Vehicle from "./components/Vehicle";
 
 export default function Home() {
   return (
     <>
       <HeroSection />
+      <Vehicle />
       <Form />
     </>
   );
